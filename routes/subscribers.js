5b63316746e3eef9f23197032623c6f50b7d8d8b
Himanshu <himanshupparmar824@gmail.com>
2023-06-10 11:20:59 +0300

    second commit

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,25 +1,36 @@
-const express = require('express')
-const router = express.Router()
-
+const express = require("express");
+const router = express.Router();
+const Subscriber = require("../models/subscriber");
 
 // Getting all
-router.get('/', (req, res) => {
-    res.send("Hello World")
-})
+router.get("/", async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find();
+    res.json(subscribers);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
 // Getting one
-router.get('/:id', (req, res) => {
-
-})
+router.get("/:id", (req, res) => {
+  res.send(req.params.id);
+});
 // Creating one
-router.post('/', (req, res) => {
-
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscibedToChannel: req.body.subscribedToChannel
+  })
+  try {
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
 })
 // Updating one
-router.patch('/', (req, res) => {
+router.patch("/:id", (req, res) => {});
+//  Deleting one
+router.patch("/", (req, res) => {});
 
-})
-//  Deleting one 
-router.patch('/', (req, res) => {
-
-})
-module.exports = router
+module.exports = router;
